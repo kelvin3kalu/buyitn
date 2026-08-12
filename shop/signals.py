3d5f05a9db8a5e647842d69e03990e40be8c0c97
kelvin <kalukelvin2024@gmail.com>
2026-08-12 22:08:24 +0100

    fix server error3

--- shop/signals.py
+++ shop/signals.py
@@ -1,42 +1,42 @@
 
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.contrib.auth.signals import user_logged_in
-from django.dispatch import receiver
-from django.core.mail import send_mail
-from django.conf import settings
+# from django.contrib.auth.models import User
+# from django.db.models.signals import post_save
+# from django.contrib.auth.signals import user_logged_in
+# from django.dispatch import receiver
+# from django.core.mail import send_mail
+# from django.conf import settings
 
 
-# Welcome email on account creation
-@receiver(post_save, sender=User)
-def send_welcome_email(sender, instance, created, **kwargs):
-    if created:
-        subject = 'Welcome to BuyIt'
-        message = f"Welcome {instance.username}, thank you for joining BuyIt!"
-        recipient_list = [instance.email]
+# # Welcome email on account creation
+# @receiver(post_save, sender=User)
+# def send_welcome_email(sender, instance, created, **kwargs):
+#     if created:
+#         subject = 'Welcome to BuyIt'
+#         message = f"Welcome {instance.username}, thank you for joining BuyIt!"
+#         recipient_list = [instance.email]
 
-        send_mail(
-            subject,
-            message,
-            settings.EMAIL_HOST_USER,
-            recipient_list,
-            fail_silently=True,
-        )
+#         send_mail(
+#             subject,
+#             message,
+#             settings.EMAIL_HOST_USER,
+#             recipient_list,
+#             fail_silently=True,
+#         )
 
 
-# Login notification email
-@receiver(user_logged_in)
-def send_login_email(sender, request, user, **kwargs):
-    subject = 'Login Notification'
-    message = f"Hello {user.username}, you just logged into your BuyIt account."
+# # Login notification email
+# @receiver(user_logged_in)
+# def send_login_email(sender, request, user, **kwargs):
+#     subject = 'Login Notification'
+#     message = f"Hello {user.username}, you just logged into your BuyIt account."
 
-    send_mail(
-        subject,
-        message,
-        settings.EMAIL_HOST_USER,
-        [user.email],
-        fail_silently=True,
-    )
+#     send_mail(
+#         subject,
+#         message,
+#         settings.EMAIL_HOST_USER,
+#         [user.email],
+#         fail_silently=True,
+#     )
 
 
 
